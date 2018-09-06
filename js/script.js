3427fd2e505cb2c2112e4944942822412c38aa38
BananaMonsterlele <39629196+BananaMonsterlele@users.noreply.github.com>
2018-09-06 17:33:50 +0500

    first commit

--- js/script.js
+++ js/script.js
@@ -108,6 +108,22 @@ window.addEventListener('DOMContentLoaded', () => {
 		})
 
 
+	//ShowMoreContent
+
+		let moreContent = document.querySelector('.button-styles'),
+			moreBlocks = document.querySelectorAll('.styles-2');
+		
+
+		moreContent.addEventListener('click', function(){
+			for(let i = 0; i < moreBlocks.length; i++){
+				moreBlocks[i].classList.remove('hidden-lg', 'hidden-md', 'hidden-sm', 'hidden-xs');
+				moreBlocks[i].classList.add('col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
+			}
+			this.style.display = `none`;
+		})
+			
+
+
 	//AJAX
 
 
