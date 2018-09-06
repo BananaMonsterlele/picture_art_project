b53c941668fcae2201c06645001cae850dbed658
BananaMonsterlele <39629196+BananaMonsterlele@users.noreply.github.com>
2018-09-06 20:41:44 +0500

    first commit

--- js/script.js
+++ js/script.js
@@ -241,5 +241,30 @@ window.addEventListener('DOMContentLoaded', () => {
 	}
 
 
+	//TimeoutForMinute
+
+
+	let overlay1 = document.querySelector('.popup-consultation'),
+		close1 = document.querySelector('.popup-close'); 
+
+
+	let timeout = setTimeout(function(){
+		
+			overlay1.style.display = 'block';
+			document.body.style.overflow = 'hidden';
+		
+	},2000)
+
+	close1.addEventListener('click', () => {
+		overlay1.style.display = 'none';
+		document.body.style.overflow = '';
+
+	});	
+	window.addEventListener('click', function(event){
+			if(overlay1.style.display == 'block' && event.target == overlay1){
+			overlay1.style.display = 'none';
+			document.body.style.overflow = '';
+		}
+		})
 
 })
