c1ce72df04019bcd86bfb5a7a11649c799360d2a
BananaMonsterlele <39629196+BananaMonsterlele@users.noreply.github.com>
2018-09-07 20:57:36 +0500

    second commit

--- js/script.js
+++ js/script.js
@@ -220,21 +220,26 @@ window.addEventListener('DOMContentLoaded', () => {
 	let block = document.getElementsByClassName('sizes-block'),
 		blockImg = document.getElementsByClassName('size-image'),
 		blockText = document.getElementsByClassName('sizes-bloc_text');
-
+		function showPic(i) {
+			blockImg[i].setAttribute('src', `img/sizes-${i+1}-${1}.png`);
+		}
+		function showText(i) {
+			blockImg[i].setAttribute('src', `img/sizes-${i+1}.png`);
+		}
 	for(let i = 0; i < block.length; i++){
 		block[i].addEventListener('mouseenter', function(){
-			blockImg[i].setAttribute('src', `img/sizes-${i+1}-${1}.png`);
+			showPic(i);
 			blockText[i].style.display = 'none';
 		})
 		block[i].addEventListener('mouseleave', function(){
-			blockImg[i].setAttribute('src', `img/sizes-${i+1}.png`);
+			showText(i);
 			blockText[i].style.display = 'block';
 		})
 		block[i].addEventListener('touchstart', function(event){
-			blockImg[i].setAttribute('src', `img/sizes-${i+1}-${1}.png`);
+			showPic(i);
 			blockText[i].style.display = 'none';
 			if(event.target != block[i]){
-				blockImg[i].setAttribute('src', `img/sizes-${i+1}.png`);
+				showText(i);
 				blockText[i].style.display = 'block';
 			}
 		})
